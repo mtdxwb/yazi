e8c9df8441332265b06c8748cf7e1ae690f82a6b
mtdxwb <zdx1314520666@163.com>
2025-04-05 11:29:13 +0800

    Update yaziline config

--- init.lua
+++ init.lua
@@ -27,10 +27,10 @@ require("yaziline"):setup({
 	separator_style = "angly", -- "angly" | "curvy" | "liney" | "empty"
 	separator_open = "",
 	separator_close = "",
-	separator_open_thin = "",
-	separator_close_thin = "",
-	separator_head = "",
-	separator_tail = "",
+	separator_open_thin = "", -- "",
+	separator_close_thin = "", --"",
+	separator_head = "▊ ",
+	separator_tail = " ▊",
 	select_symbol = "",
 	yank_symbol = "󰆐",
 	filename_max_length = 24, -- truncate when filename > 24
