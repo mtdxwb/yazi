3c2416535f8078c5cec22c0e821bbdd8d1533d53
mtdxwb <zdx1314520666@163.com>
2025-04-12 21:42:49 +0800

    Remove yaziline.yazi | Add no-status

--- init.lua
+++ init.lua
@@ -21,26 +21,11 @@ require("eza-preview"):setup({
 	dereference = false,
 })
 
--- yaziline plugin
-require("yaziline"):setup({
-	-- color = "#98c379", -- main theme color
-	separator_style = "angly", -- "angly" | "curvy" | "liney" | "empty"
-	separator_open = "",
-	separator_close = "",
-	separator_open_thin = "", -- "",
-	separator_close_thin = "", --"",
-	separator_head = "▊ ",
-	separator_tail = " ▊",
-	select_symbol = "",
-	yank_symbol = "󰆐",
-	filename_max_length = 24, -- truncate when filename > 24
-	filename_truncate_length = 6, -- leave 6 chars on both sides
-	filename_truncate_separator = "...", -- the separator of the truncated filename
-})
-
 -- git.yazi plugin
 require("git"):setup()
 
+-- no-status
+require("no-status"):setup()
 -- toggle pane plugin
 -- if os.getenv("NVIM") then
 -- require("toggle-pane"):entry("max-preview")
